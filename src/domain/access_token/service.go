5a50d97446c119190d3d6e3bedd4db1ba3f05ccf
Evgenii Myshevskii <e.g.myshevskiy@gmail.com>
2023-04-18 02:55:35 +0300

    add some validation to accessTokenId

--- src/domain/access_token/service.go
+++ src/domain/access_token/service.go
@@ -1,6 +1,8 @@
 package access_token
 
 import (
+	"strings"
+
 	"github.com/MrBonzzo/udm-bs-oauth-api/src/utils/errors"
 )
 
@@ -17,6 +19,11 @@ type service struct {
 }
 
 func (s *service) GetById(accessTokenId string) (*AccessToken, *errors.RestErr) {
+	accessTokenId = strings.TrimSpace(accessTokenId)
+	if len(accessTokenId) == 0 {
+		return nil, errors.NewBadRequestError("invalid access token id")
+	}
+
 	accessToken, err := s.repository.GetById(accessTokenId)
 	if err != nil {
 		return nil, err
